6eaa5632ead43784f14a94cfaa2153c3d0bd47f1
Andrew Presnell <andrew_presnell@hotmail.com>
2018-05-30 09:47:22 -0700

    Delete user-info.js

--- commands/user-info.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'user-info',
-  description: 'User-Info!',
-  execute(message, args) {
-    message.channel.send(`Your username: ${message.author.username}
-    Your ID: ${message.author.id}`);
-  },
-};
